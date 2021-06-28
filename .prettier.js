595894f7d64732193e1c9726252415883d91ce48
Armando Bibay <armandobibayjr@Armandos-MacBook-Pro.local>
2021-06-28 22:05:07 +0800

    stage all

---
+++ .prettier.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 80,
+  bracketSpacing: false,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  semi: true,
+  endOfLine: "crlf",
+};
--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import './App.css';
-import validate from 'hooks/useValidator'
-import { isEmailAddress, isMobileNumber} from 'helpers/regex.helper';
+import "./App.css";
+import validate, { isEmailAddress, isMobileNumber } from "hooks";
 
 function App() {
-
   const fields = {
     name: {
       isRequired: true,
-      validator: isEmailAddress
+      validator: isEmailAddress,
+      errorMessage: "invalid Email Address",
     },
     address: {
-      isRequired:false,
-      validator: isMobileNumber
-    }
-  }
-  const {state, updateFieldValue, isDisabled} =validate(fields)
+      isRequired: false,
+      validator: isMobileNumber,
+      errorMessage: "invalid Mobile Number",
+    },
+  };
+  const { state, updateFieldValue, isDisabled } = validate(fields);
 
- 
-const handleButtonClick =(e)=>{
+  const handleButtonClick = (e) => {};
 
-}
-
-
-const handleOnChange=(e)=>{
-  const {id, value} = e.target
-  updateFieldValue(id, value)
-}
+  const handleOnChange = (e) => {
+    const { id, value } = e.target;
+    updateFieldValue(id, value);
+  };
   return (
-    <div className="App">
+    <div className='App'>
       <form>
-        <input id="name" type="text" onChange={handleOnChange} /> <br/>
-        <p>{state['name'].error}</p>
-        <input id="address" type="text" onChange={handleOnChange} /><br/>
-        <p>{state['address'].error}</p>
-        <button type='button' title="check" onClick={handleButtonClick}>{isDisabled?'disabled':'Congrats'}</button>
+        <input id='name' type='text' onChange={handleOnChange} /> <br />
+        <p>{state["name"].error}</p>
+        <input id='address' type='text' onChange={handleOnChange} />
+        <br />
+        <p>{state["address"].error}</p>
+        <button type='button' title='check' onClick={handleButtonClick}>
+          {isDisabled ? "disabled" : "Congrats"}
+        </button>
       </form>
-    
-
     </div>
   );
 }
